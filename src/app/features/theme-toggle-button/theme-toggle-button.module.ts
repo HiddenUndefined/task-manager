fa66b5de2127b0a550896c1e4388939dfe06e6a5
HiddenUndefined <georgerudenko97@gmail.com>
2023-04-01 01:14:38 +0300

    feat: update theme-toggle-button component

--- src/app/features/theme-toggle-button/theme-toggle-button.module.ts
+++ src/app/features/theme-toggle-button/theme-toggle-button.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core'
 // Spaces
 import { ThemeToggleButtonComponent } from './theme-toggle-button.component'
+import { FeatherIconsModule } from '@shared/vendor/feather-icons/feather-icons.module'
 
 @NgModule({
   declarations: [
     ThemeToggleButtonComponent
   ],
+  imports: [
+    FeatherIconsModule
+  ],
   exports: [
     ThemeToggleButtonComponent
-  ],
-  imports: []
+  ]
 })
 export class ThemeToggleButtonModule {
 }

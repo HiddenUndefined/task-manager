262d22b58dd10964536a175d0ed3095b0982a60e
HiddenUndefined <georgerudenko97@gmail.com>
2023-03-31 23:48:09 +0300

    refactor: global style and layouts styles

--- src/app/layouts/website/website.component.ts
+++ src/app/layouts/website/website.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-website',
+  selector: 'app-website-layout',
   templateUrl: 'website.component.html',
   styleUrls: ['website.component.sass']
 })

07463379071856f3899225ad7ab4490ece72f718
HiddenUndefined <georgerudenko97@gmail.com>
2023-04-06 22:35:11 +0300

    refactor: use :host in component style

--- src/app/pages/auth/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/auth/pages/forgot-password/forgot-password.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-forgot-password',
-  templateUrl: 'forgot-password.component.html'
+  templateUrl: 'forgot-password.component.html',
+  styleUrls: []
 })
 export class ForgotPasswordComponent {
 
--- src/app/pages/auth/pages/sign-in/sign-in.component.ts
+++ src/app/pages/auth/pages/sign-in/sign-in.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-sign-in',
-  templateUrl: 'sign-in.component.html'
+  templateUrl: 'sign-in.component.html',
+  styleUrls: ['sign-in.component.sass']
 })
 export class SignInComponent implements OnInit {
   ngOnInit () {
